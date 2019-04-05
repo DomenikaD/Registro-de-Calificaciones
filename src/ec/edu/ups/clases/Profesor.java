a4b57f049feaae5312bacfbccf4adfcd3256f083
Domenika Delgado <Domenika Delgado@Pc_Hp>
2019-04-05 10:24:23 -0500

    Practica01

--- src/ec/edu/ups/clases/Profesor.java
+++ src/ec/edu/ups/clases/Profesor.java
@@ -11,4 +11,39 @@ package ec.edu.ups.clases;
  */
 public class Profesor {
     
+    private String titulo;
+    private double salario;
+    private String cargo;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    @Override
+    public String toString() {
+        return "Profesor{" + "titulo=" + titulo + ", salario=" + salario + ", cargo=" + cargo + '}';
+    }
+    
+    
+    
 }
